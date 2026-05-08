9a07ec4e44d292bda3e1f845e1dc4efd5c3e495c
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-05-08 14:02:24 +0530

    fix: update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://crmbackend.pasindujayarathna.me/api",
+  //http://localhost:3000
 });
 
 api.interceptors.request.use((config) => {
