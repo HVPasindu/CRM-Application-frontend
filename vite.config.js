b81ed4782c3571c1061e8da4b2e4be95c17b3736
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-05-08 15:03:01 +0530

    feat: add GitHub Actions workflow for deploying frontend to EC2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:'./',
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
